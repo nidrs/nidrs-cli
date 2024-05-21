18aba72489601de10b7ed72f40bff21df09da269
WumaCoder <1829913225@qq.com>
2024-05-22 00:20:29 +0800

    chore: add cli test

--- nidrs-cli/src/main.rs
+++ nidrs-cli/src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_cmd::{output::OutputOkExt, Command};
+
+    #[test]
+    fn test_hello() {
+        let mut cmd = Command::cargo_bin("nidrs-cli").unwrap();
+        cmd.arg("--name").arg("Alice").arg("--count").arg("3");
+        cmd.assert().success();
+        // insta::assert_debug_snapshot!(cmd.output());
+    }
 }

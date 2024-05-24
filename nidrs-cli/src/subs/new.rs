34eb7bb024a4a4463f0b76b82e5809326631f85c
刘欢 <1829913225@qq.com>
2024-05-24 20:08:05 +0800

    feat: 细节完成

--- nidrs-cli/src/subs/new.rs
+++ nidrs-cli/src/subs/new.rs
@@ -1,5 +1,10 @@
-use std::{any, path::PathBuf};
+use std::{
+    any,
+    io::{BufRead, Write},
+    path::PathBuf,
+};
 
+use serde::de::Error;
 use toml::Table;
 
 /// eg: nid new . --name=project_name --template=template_name --yes
@@ -84,29 +89,73 @@ impl NewCommand {
         }
 
         // git clone
-        std::process::Command::new("git")
-            .arg("clone")
-            .arg(&template_url)
-            .arg(&project_path)
-            .output()
-            .expect("failed to execute process");
+        exec_cmd(
+            "git",
+            std::process::Command::new("git")
+                .arg("clone")
+                .arg(&template_url)
+                .arg(&project_path),
+        )
+        .unwrap();
 
         // remove .git
         let git_path = project_path.join(".git");
 
         let _ = std::fs::remove_dir_all(git_path);
 
-        if !we.is_init_git() || true {
+        if !we.is_init_git() {
             // git init and print
-            let out = std::process::Command::new("git")
+            std::process::Command::new("git")
                 .arg("init")
                 .current_dir(&project_path)
+                .stdout(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
                 .output()
                 .expect("failed to execute process");
         }
 
         println!("success!")
     }
+}
+
+fn exec_cmd(prefix: &str, cmd: &mut std::process::Command) -> Result<(), anyhow::Error> {
+    let mut child = cmd
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .spawn()
+        .expect("failed to execute process");
+
+    if let Some(stderr) = child.stderr.take() {
+        let reader = std::io::BufReader::new(stderr);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                eprintln!("[{}] {}", prefix, line);
+            }
+        }
+    }
+
+    if let Some(stdout) = child.stdout.take() {
+        let reader = std::io::BufReader::new(stdout);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                println!("[{}] {}", prefix, line);
+            }
+        }
+    }
+
+    let status = child.wait().expect("failed to wait for process");
+
+    // 检查命令是否成功执行
+    if !status.success() {
+        // 如果失败，打印错误消息
+        eprintln!("[{}] command failed with error: {}", prefix, status);
+        return Err(anyhow::Error::msg("command failed"));
+    }
+
+    // std::io::stdout().flush().expect("Failed to flush stdout");
+    // std::io::stderr().flush().expect("Failed to flush stderr");
+
+    Ok(())
 }
 
 #[derive(Debug)]

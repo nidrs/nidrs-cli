fb72938d6fa108300afb9fc02846fd01149ede10
Lydanne <1829913225@qq.com>
2024-09-22 23:39:33 +0800

    chore: 添加例子

--- examples/openapi-client-js-demo/src/fetchDemo.ts
+++ examples/openapi-client-js-demo/src/fetchDemo.ts
@@ -1,8 +1,11 @@
 import api from "./api";
 
 export function fetchDemo(element: HTMLButtonElement) {
-  element.addEventListener("click", () => {
-    api.user.get_all({}).then((res) => {
+  element.addEventListener("click", async () => {
+    await api.user.get_all({}).then((res) => {
+      console.log(res);
+    });
+    await api.app.get_hello_world({}).then((res) => {
       console.log(res);
     });
   });
